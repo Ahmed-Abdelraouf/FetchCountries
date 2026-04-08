2cbf895c2ab4a48b72f4c7a76db7daaf0140f077
Ahmed Abdelraouf <aabdelraouf@ntgclarity.com>
2026-04-08 21:27:26 +0200

    Initial Commit

---
+++ CountiresTask/CountiresTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountiresTaskApp.swift
+//  CountiresTask
+//
+//  Created by NTG on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CountiresTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
